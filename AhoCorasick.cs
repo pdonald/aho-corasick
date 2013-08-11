b188af89ced04b14f03088abff2bcbe413201eac
Pēteris Ņikiforovs <p@mak.id.lv>
2013-08-11 22:30:31 +0300

    Initial commit

---
+++ AhoCorasick.cs
@@ -0,0 +1,175 @@
+﻿// Copyright (c) 2013 Pēteris Ņikiforovs
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AhoCorasick
+{
+    public class Trie : Trie<string>
+    {
+        public void Add(string keyword)
+        {
+            Add(keyword, keyword);
+        }
+
+        public void Add(IEnumerable<string> keywords)
+        {
+            foreach (string keyword in keywords)
+            {
+                Add(keyword);
+            }
+        }
+    }
+
+    public class Trie<TValue> : Trie<char, TValue>
+    {
+    }
+
+    public class Trie<TKeyword, TValue>
+    {
+        private readonly Node<TKeyword, TValue> root = new Node<TKeyword, TValue>();
+
+        public void Add(IEnumerable<TKeyword> keyword, TValue value)
+        {
+            var node = root;
+
+            foreach (TKeyword c in keyword)
+            {
+                var child = node[c];
+
+                if (child == null)
+                    child = node[c] = new Node<TKeyword, TValue>(c, node);
+
+                node = child;
+            }
+
+            node.Values.Add(value);
+        }
+
+        public void Build()
+        {
+            var queue = new Queue<Node<TKeyword, TValue>>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                foreach (var child in node)
+                    queue.Enqueue(child);
+
+                if (node == root)
+                {
+                    root.Fail = root;
+                    continue;
+                }
+
+                var fail = node.Parent.Fail;
+
+                while (fail[node.Keyword] == null && fail != root)
+                    fail = fail.Fail;
+
+                node.Fail = fail[node.Keyword] ?? root;
+                if (node.Fail == node) 
+                    node.Fail = root;
+            }
+        }
+
+        public IEnumerable<TValue> Find(IEnumerable<TKeyword> text)
+        {
+            var node = root;
+
+            foreach (TKeyword c in text)
+            {
+                while (node[c] == null && node != root)
+                    node = node.Fail;
+
+                node = node[c] ?? root;
+
+                for (var t = node; t != root; t = t.Fail)
+                {
+                    foreach (TValue value in t.Values)
+                        yield return value;
+                }
+            }
+        }
+
+        private class Node<TNodeKeyword, TNodeValue> : IEnumerable<Node<TNodeKeyword, TNodeValue>>
+        {
+            private readonly TNodeKeyword keyword;
+            private readonly Node<TNodeKeyword, TNodeValue> parent;
+            private readonly Dictionary<TNodeKeyword, Node<TNodeKeyword, TNodeValue>> children = new Dictionary<TNodeKeyword, Node<TNodeKeyword, TNodeValue>>();
+            private readonly List<TNodeValue> values = new List<TNodeValue>();
+
+            public Node()
+            {
+            }
+
+            public Node(TNodeKeyword keyword, Node<TNodeKeyword, TNodeValue> parent)
+            {
+                this.keyword = keyword;
+                this.parent = parent;
+            }
+
+            public TNodeKeyword Keyword
+            {
+                get { return keyword; }
+            }
+
+            public Node<TNodeKeyword, TNodeValue> Parent
+            {
+                get { return parent; }
+            }
+
+            public Node<TNodeKeyword, TNodeValue> Fail
+            {
+                get;
+                set;
+            }
+
+            public Node<TNodeKeyword, TNodeValue> this[TNodeKeyword c]
+            {
+                get { return children.ContainsKey(c) ? children[c] : null; }
+                set { children[c] = value; }
+            }
+
+            public List<TNodeValue> Values
+            {
+                get { return values; }
+            }
+
+            public IEnumerator<Node<TNodeKeyword, TNodeValue>> GetEnumerator()
+            {
+                return children.Values.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            public override string ToString()
+            {
+                return Keyword.ToString();
+            }
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) 2013 Pēteris Ņikiforovs
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace AhoCorasick
+{
+    public class Tests
+    {
+        [Test]
+        public void HelloWorld()
+        {
+            string text = "hello and welcome to this beautiful world!";
+
+            AhoCorasick.Trie trie = new AhoCorasick.Trie();
+            trie.Add("hello");
+            trie.Add("world");
+            trie.Build();
+
+            string[] matches = trie.Find(text).ToArray();
+
+            Assert.AreEqual(2, matches.Length);
+            Assert.AreEqual("hello", matches[0]);
+            Assert.AreEqual("world", matches[1]);
+        }
+
+        [Test]
+        public void Contains()
+        {
+            string text = "hello and welcome to this beautiful world!";
+
+            AhoCorasick.Trie trie = new AhoCorasick.Trie();
+            trie.Add("hello");
+            trie.Add("world");
+            trie.Build();
+
+            Assert.IsTrue(trie.Find(text).Any());
+        }
+
+        [Test]
+        public void LineNumbers()
+        {
+            string text = "world, i hello you!";
+            string[] words = new[] { "hello", "world" };
+
+            AhoCorasick.Trie<int> trie = new AhoCorasick.Trie<int>();
+            for (int i = 0; i < words.Length; i++)
+                trie.Add(words[i], i);
+            trie.Build();
+
+            int[] lines = trie.Find(text).ToArray();
+
+            Assert.AreEqual(2, lines.Length);
+            Assert.AreEqual(1, lines[0]);
+            Assert.AreEqual(0, lines[1]);
+        }
+
+        [Test]
+        public void Words()
+        {
+            string[] text = "one two three four".Split(' ');
+            
+            AhoCorasick.Trie<string, bool> trie = new AhoCorasick.Trie<string, bool>();
+            trie.Add(new[] { "three", "four" }, true);
+            trie.Build();
+
+            Assert.IsTrue(trie.Find(text).Any());
+        }
+    }
+}
